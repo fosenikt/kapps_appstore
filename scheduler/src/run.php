1aadf48234dff2a2de5209079fa4c40e64daae06
dico <ra@stadsbygd.net>
2022-12-15 09:21:45 +0100

    Added check for ssl in scheduler

--- scheduler/src/run.php
+++ scheduler/src/run.php
@@ -1,7 +1,11 @@
 <?php
 require_once('functions.php');
 
-$api_base_url = 'https://'.getenv("API_URL");
+if (getenv("SSL") !== null && getenv("SSL") == 'true') {
+	$api_base_url = 'https://'.getenv("API_URL");
+} else {
+	$api_base_url = 'https://'.getenv("API_URL");
+}
 $api_token = getenv("TOKEN");
 
 $schedule = CallAPI('GET', '/mail/send', false);
